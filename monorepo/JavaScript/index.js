b253071c6ab4a3edd70619c051c690f9a1756644
Fatbardha2002 <fatbardhacocaj@gmail.com>
2023-09-07 13:22:15 +0200

    Added functionality - shuffleBtn , showBtn

--- monorepo/JavaScript/index.js
+++ monorepo/JavaScript/index.js
@@ -9,9 +9,23 @@ document.addEventListener("DOMContentLoaded", function () {
     changeBtn.addEventListener('click', changeValues);
     showBtn.addEventListener('click', showSelectedValues);
 
+    function shuffleValues() {
+        const values = Array.from(checkboxes).map(checkbox => checkbox.value);
+        shuffleArray(values);
+        updateCheckboxes(values);
+        displayResult(`Shuffled values: ${values.join(', ')}`);
+    }
+
     function changeValues() {
         const newValues = ["Junior", "React", "Developer", "Pabau2023"];
         updateCheckboxes(newValues);
         displayResult('Changed values: ' + newValues.join(', '));
+    }
+
+    function showSelectedValues() {
+        const selectedValues = Array.from(checkboxes)
+            .filter(checkbox => checkbox.checked)
+            .map(checkbox => checkbox.value);
+        displayResult('Selected values: ' + selectedValues.join(', '));
     }
 });
