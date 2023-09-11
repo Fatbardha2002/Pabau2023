8031d95f7ab10665e85c532ec61992416edc435e
Fatbardha2002 <fatbardhacocaj@gmail.com>
2023-09-11 13:47:19 +0200

    Added solution for TypeScript Task

---
+++ monorepo/TypeScript/types.d.ts
@@ -0,0 +1,26 @@
+
+interface Intern {
+    name: string;
+    age: number;
+    skills: string[];
+  }
+  
+  interface Junior {
+    name: string;
+    age: number;
+    skills: string[];
+    date_of_promotion: Date;
+    domain: DomainType;
+  }
+
+  enum DomainType {
+    Web = "Web",
+    Connect = "Connect",
+    Backend = "Backend",
+  }
+
+  interface Company {
+    Interns: Intern[];
+    Juniors: Junior[];
+    Country: string; 
+  }
